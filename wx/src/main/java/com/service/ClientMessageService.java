6fe50499ca21f1255e3e20219b4403a137f53f91
Administrator <Administrator@IPATN63BGW6DPDG>
2019-02-21 11:18:57 +0800

    1

--- wx/src/main/java/com/service/ClientMessageService.java
+++ wx/src/main/java/com/service/ClientMessageService.java
@@ -1,11 +1,10 @@
 package com.service;
 
 import com.config.ClientConfig;
-import com.message.IMessage;
 
 public interface ClientMessageService {
 
 	String doService(ClientConfig clientConfig,String message);
+
 	
-	IMessage doService(ClientConfig clientConfig,IMessage message);
 }
