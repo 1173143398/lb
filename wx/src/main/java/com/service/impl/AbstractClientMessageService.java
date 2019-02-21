8dff42d76e1b459a6da7ea46a73f7c227f9edfa2
Administrator <Administrator@IPATN63BGW6DPDG>
2019-02-21 11:19:33 +0800

    2

--- wx/src/main/java/com/service/impl/AbstractClientMessageService.java
+++ wx/src/main/java/com/service/impl/AbstractClientMessageService.java
@@ -1,13 +1,8 @@
 package com.service.impl;
 
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-
 import org.springframework.beans.factory.annotation.Autowired;
 
 import com.config.ClientConfig;
-import com.context.TransactionContext;
-import com.execpt.WxException;
 import com.message.IMessage;
 import com.parse.IContentParser;
 import com.parse.IWxExpressionParser;
@@ -27,26 +22,7 @@ public abstract class AbstractClientMessageService implements
 	protected IWxExpressionParser wxExpressionParser;
 	
 	protected String formatUrl(String url,IMessage reqParam) {
-		Pattern compile = Pattern.compile("(\\$\\{\\w+\\})");
-		Matcher matcher = compile.matcher(url);
-		while(matcher.find()){
-			String param = matcher.group();
-			String pName = param.replaceAll("[\\$|\\{|\\}]", "");
-			String value = null;
-			try{
-				value = wxExpressionParser.getValue(TransactionContext.getSystemConfig(), pName);
-				if(StringUtil.isNull(value)){
-					value = wxExpressionParser.getValue(reqParam, pName);
-				}
-			}catch(Exception e){
-				value = wxExpressionParser.getValue(reqParam, pName);
-			}
-			if(StringUtil.isNull(value)){
-				throw new WxException("获取参数值失败" + pName);
-			}
-			url = url.replace(param, value);
-		}
-		return url;
+		return StringUtil.formatUrl(wxExpressionParser, url, reqParam);
 	}
 
 	@Override
